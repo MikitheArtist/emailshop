ea80190032f75cb619d8bd5c3a9f97bceeaf9f9d
MikitheArtist <56101985+MikitheArtist@users.noreply.github.com>
2020-01-26 20:49:13 +0200

    get10%

--- src/scripts/models/catalog.js
+++ src/scripts/models/catalog.js
@@ -1,6 +1,6 @@
 
 export const productsCatalog = [
-    {id:'el1', name:'ladies fashion bag', price:30.00, img:'/src/img/bag1.png', quantity: 1},
-    {id:'el2', name:'Leather fashiionable bag', price:30.00, img:'/src/img/bag2.png', quantity: 1},
-    {id:'el3', name:'Leather fashiionable bag', price:30.00, img:'/src/img/bag3.png', quantity: 1},
+    {id:'el1', title:'ladies fashion bag', price:30.00, img:'/src/img/bag1.png', quantity: 3},
+    {id:'el2', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag2.png', quantity: 10},
+    {id:'el3', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag3.png', quantity: 11},
 ];
