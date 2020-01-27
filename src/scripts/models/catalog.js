eef85f973774eaf1371d8e281e283d0cd052ae13
MikitheArtist <56101985+MikitheArtist@users.noreply.github.com>
2020-01-27 13:30:37 +0200

    Добавляем картинки

--- src/scripts/models/catalog.js
+++ src/scripts/models/catalog.js
@@ -1,6 +1,12 @@
 
 export const productsCatalog = [
-    {id:'el1', title:'ladies fashion bag', price:30.00, img:'/src/img/bag1.png', quantity: 3},
-    {id:'el2', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag2.png', quantity: 10},
-    {id:'el3', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag3.png', quantity: 11},
+    {id:'el1', title:'ladies fashion bag', price:30.00, img:'/src/img/bag1.png', quantity: 3, is_new: true},
+    {id:'el2', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag2.png', quantity: 10, is_new: true},
+    {id:'el3', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag3.png', quantity: 11, is_new: true},
+    {id:'el3', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag4.png', quantity: 11,is_featured: true},
+    {id:'el3', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag5.png', quantity: 11, is_featured: true},
+    {id:'el3', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag6.png', quantity: 11, is_featured: true},
+    {id:'el3', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag7.png', quantity: 11,is_featured: true},
+    {id:'el3', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag8.png', quantity: 11, is_featured: true},
+    {id:'el3', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag9.png', quantity: 11, is_featured: true}
 ];
