29370e7c778f4fd91ffdf115f902b1d8315bdde8
MikitheArtist <56101985+MikitheArtist@users.noreply.github.com>
2020-01-30 16:37:01 +0200

    alert hi

--- src/scripts/models/catalog.js
+++ src/scripts/models/catalog.js
@@ -1,12 +1,12 @@
 
 export const productsCatalog = [
-    {id:'el1', title:'ladies fashion bag', price:30.00, img:'/src/img/bag1.png', quantity: 3, is_new: true},
-    {id:'el2', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag2.png', quantity: 10, is_new: true},
-    {id:'el3', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag3.png', quantity: 11, is_new: true},
-    {id:'el4', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag4.png', quantity: 11,is_featured: true},
-    {id:'el5', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag5.png', quantity: 11, is_featured: true},
-    {id:'el6', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag6.png', quantity: 11, is_featured: true},
-    {id:'el7', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag7.png', quantity: 11,is_featured: true},
-    {id:'el8', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag8.png', quantity: 11, is_featured: true},
-    {id:'el9', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag9.png', quantity: 11, is_featured: true}
+    {id:'el1', title:'ladies fashion bag', price:30.00, img:'/src/img/bag1.png', quantity: 20, is_new: true},
+    {id:'el2', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag2.png', quantity: 20, is_new: true},
+    {id:'el3', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag3.png', quantity: 20, is_new: true},
+    {id:'el4', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag4.png', quantity: 20,is_featured: true},
+    {id:'el5', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag5.png', quantity: 20, is_featured: true},
+    {id:'el6', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag6.png', quantity: 20, is_featured: true},
+    {id:'el7', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag7.png', quantity: 20,is_featured: true},
+    {id:'el8', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag8.png', quantity: 20, is_featured: true},
+    {id:'el9', title:'Leather fashiionable bag', price:30.00, img:'/src/img/bag9.png', quantity: 20, is_featured: true}
 ];
