408dbd647ae93086334720899bc5fcf33cb374f9
MikitheArtist <56101985+MikitheArtist@users.noreply.github.com>
2020-02-02 15:33:29 +0200

    попытка 1

--- src/scripts/cart/CartRenderer.js
+++ src/scripts/cart/CartRenderer.js
@@ -1,3 +1,5 @@
+import Mustache from 'mustache';
+
 export default class CartRenderer {
     constructor(cart, {
         headerCartSelect = '.header-tools-item'
@@ -8,5 +10,37 @@ export default class CartRenderer {
 
     updateHeaderCart() {
         this.headerCartEl.querySelector('.header-tools-item-badge').innerHTML = this.cart.cartItems.length;
+        this.assignListener();
     }
+
+
+    assignListener() {
+        
+
+        document.querySelector('.header-tools-item').addEventListener('click', () => {
+
+            let cartDiv = document.querySelector('.hello-div');
+
+            cartDiv.innerHTML = '';
+
+            let productTMPL = document.getElementById('cart-item').innerHTML;
+
+            this.cart.cartItems.forEach((cartItem) => {
+                cartDiv.innerHTML += Mustache.render(productTMPL, cartItem);
+            });
+
+            document.getElementsByClassName('hello-div-content').classList.add("hello-div-content-active");
+
+            document.querySelector('.close').addEventListener('click', () => {
+
+                document.getElementsByClassName('hello-div-content').classList.remove("hello-div-content-active");
+            });
+
+        });
+
+
+    }
+
+
+
 }
