60c2caca393313c1564d11ce07dd1279672f3282
MikitheArtist <56101985+MikitheArtist@users.noreply.github.com>
2020-02-13 15:13:40 +0200

    error_this

--- src/scripts/cart/CartRenderer.js
+++ src/scripts/cart/CartRenderer.js
@@ -30,46 +30,20 @@ export default class CartRenderer {
                 cartDiv.innerHTML += Mustache.render(productTMPL, cartItem);
             });
 
-           /* Array.from(document.querySelectorAll('.remove_button')).forEach(element => {
-                element.addEventListener('click', (e) => {
-                    let productItem = e.target.closest('.product-item');
-
-                    console.log(productItem.dataset.id);
-
-                    if (productItem) {
-                        this.cart.removeProduct(productItem.dataset.id);
-
-                    }
-
-                });
-            });*/
-
-           /*let helloDiv = document.querySelector('.hello-div');
-            helloDiv.addEventListener('click', (e) => {
-
-               if (e.target.tagName === 'button') {
-                   if (e.target.classList.contains('remove_button')) {
-                       let target = document.querySelector('.product-item');
-                       console.log(target);
-                       this.cart.removeProduct(target.dataset.id);
-                   }
-               }
-           });*/
-
-            document.addEventListener('click', function (event) {
-                if (event.target.tagName === 'button') {
-                    if (event.target.classList.contains('remove_button')) {
-                        let target = event.target.closest('.product-item');
-                        console.log(target);
-                        this.cart.removeProduct(target.dataset.id);
-                    }
-                }
-
-            });
-
-
 
             document.querySelector('.hello-div-content').classList.add("hello-div-content-active");
+
+        });
+
+
+        document.addEventListener('click', function (event) {
+            if (event.target.tagName === 'button') {
+                if (event.target.classList.contains('remove_button')) {
+                    let target = event.target.closest('.product-item');
+                    console.log(target);
+                    this.cart.removeProduct(target.dataset.id);
+                }
+            }
 
         });
 
